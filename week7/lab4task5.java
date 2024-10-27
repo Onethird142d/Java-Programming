7612adf5a05b2e944b8a5c0e6b5214cdc5e31e8a
Onethird142d <944697110@qq.com>
2024-10-28 01:41:46 +0800

    Add files via upload

---
+++ week7/lab4task5.java
@@ -0,0 +1,11 @@
+public class lab4task5 {
+    public static void nPrintln (String message,int n){
+            for (int i = 0; i < n; i++) {
+                System.out.println(message);
+            }
+        }
+        public static void main(String[] args){
+        nPrintln("Welcome to java",5);
+        }
+    }
+//这个不是很清楚
